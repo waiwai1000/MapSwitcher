b4bde2a435aac8e9dbaaeb1a12f488f8d512089b
ZhiJianTan <45629781+ZhiJianTan@users.noreply.github.com>
2019-11-04 18:54:41 +0800

    Revert "Add files via upload"
    This reverts commit 38e2cb7b056739c5972de77a27fa3c912bae7d9a.

--- src/maps.js
+++ src/maps.js
@@ -103,45 +103,6 @@ const maps = [{
       } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),satellite/)){
         let [,lat,lon,zoom] = match;
         return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),terrain/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),public_transport/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),traffic/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      }
-    },
-  },
-  {
-    name: "Wikimapia",
-    category: MAIN_CATEGORY,
-    domain: "http://wikimapia.org/",
-    getUrl(lat, lon, zoom) {
-      return 'http://wikimapia.org/#lang=en&lat=' + lat + '&lon=' + lon + '&z=' + zoom+',&m=w';
-    },
-    getLatLonZoom(url) {
-      let match;
-      if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),normal&x=ep/)) {
-        const [, lat, lon, zoom] = match;
-        return [lat, lon, zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),normal/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),satellite/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),terrain/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),public_transport/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
-      } else if (match = url.match(/wego\.here\.com.*map=(-?\d[0-9.]*),(-?\d[0-9.]*),(\d{1,2}),traffic/)){
-        let [,lat,lon,zoom] = match;
-        return [lat,lon,zoom];
       }
     },
   },
